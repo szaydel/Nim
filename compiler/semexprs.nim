8d0e853e0afc7d0c4830cb7e03dec88c5c814aef
metagn <metagngn@gmail.com>
2025-01-15 22:01:56 +0300

    ignore match errors to expected types of tuple constructor elements (#24611)
    fixes #24609
    
    A tuple may have an incompatible expected type if there is a converter
    match to it. So the compiler should not error when trying to match the
    individual elements in the constructor to the elements of the expected
    tuple type, this will be checked when the tuple is entirely constructed
    anyway.

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -2929,7 +2929,10 @@ proc semTupleFieldsConstr(c: PContext, n: PNode, flags: TExprFlags; expectedType
       # hasEmpty/nil check is to not break existing code like
       # `const foo = [(1, {}), (2, {false})]`,
       # `const foo = if true: (0, nil) else: (1, new(int))`
-      n[i][1] = fitNode(c, expectedElemType, n[i][1], n[i][1].info)
+      let conversion = indexTypesMatch(c, expectedElemType, n[i][1].typ, n[i][1])
+      # ignore matching error, full tuple will be matched later which may call converter, see #24609
+      if conversion != nil:
+        n[i][1] = conversion
 
     if n[i][1].typ.kind == tyTypeDesc:
       localError(c.config, n[i][1].info, "typedesc not allowed as tuple field.")
